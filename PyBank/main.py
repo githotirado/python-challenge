2018b47b9d5520a7ee47e02829fc4e139d38b8ba
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2021-07-10 22:16:28 -0700

    code is functional. Need to clean up and format

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,20 @@
 # import modules
 import csv, pathlib
 
-# initialize list to store dictionary rows and moving increases/decreases
+# initialize list to store dictionary rows and profit/loss changes
 budget_list = []
-profit_loss_total = 0
-profit_change_total = 0
-greatest_decrease = 0
-greatest_increase = 0
-profit_loss_change = 0
-prior_profit_loss = 0
 
-# read in the file
-budget_csv = pathlib.Path("./Resources/budget_data.csv")
+# initialize counter variables to zero
+profit_loss_total   = 0
+profit_change_total = 0
+greatest_decrease   = 0
+greatest_increase   = 0
+profit_loss_change  = 0
+prior_profit_loss   = 0
+
+# Define input and output files
+budget_csv      = pathlib.Path("./Resources/budget_data.csv")
+budget_analysis = pathlib.Path("./analysis/budget_analysis.csv")
 
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
@@ -19,12 +22,13 @@ with open(budget_csv) as csvfile:
     csvheader = next(csvreader)
 
     # Read in the rest of the file
-    # Save into a list of dictionaries
-    #    records  =  [ 0, {"Jan-2010": 867884}]
+    # Gather stats per line, save into a list of dictionaries
     for index, row in enumerate(csvreader):
         profit_loss = float(row[1])
         profit_loss_date = row[0]
-        profit_loss_total += profit_loss
+
+        # Calculate change in profit/loss for current row
+        # First entry has no profit/loss change
         if index == 0:
             profit_loss_change = 0
         else:
@@ -39,25 +43,34 @@ with open(budget_csv) as csvfile:
             greatest_decrease = profit_loss_change
             greatest_decrease_date = profit_loss_date
 
+        # Store rows in list of dictionaries. Include change in profit/loss
         budget_list.append({profit_loss_date : [profit_loss, profit_loss_change]})
         
+        # Update counters for next iteration
         prior_profit_loss    = profit_loss
         profit_change_total += profit_loss_change
+        profit_loss_total   += profit_loss
 
 # Count number of months in data set
 months_count = len(budget_list)
 
+# for irow in budget_list:
+#     print(f"{irow}")
+
 # The net total of profit/losses over entire period
-## already calculated in final profit_losses
+## already calculated in profit_loss_total
 
 # The average of changes over entire period (exclude first row)
 avg_change = profit_change_total / (months_count - 1)
 
-# Greatest increase in profits (date and amount)
-# Greatest decrease in losses (date and amount)
-## print(budget_dict)
+# Print out findings to screen
+print("---------------------------------------------------------")
+print(f"Financial Analysis for file {budget_csv}")
+print("---------------------------------------------------------")
 print(f"Total Months:                       {months_count}")
 print(f"Total Profit/Loss:                  ${profit_loss_total:.0f}")
 print(f"Average Change:                     ${avg_change:.2f}")
 print(f"Greatest Profits Increase: {greatest_increase_date} ${greatest_increase:.0f}")
-print(f"Greatest Profits Decrease: {greatest_decrease_date} ${greatest_decrease:.0f}")
+print(f"Greatest Profits Decrease: {greatest_decrease_date} ${greatest_decrease:.0f}\n")
+
+# Print findings to analysis folder
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,33 +1,42 @@
-import csv
+import csv, pathlib
 
-poll_file = './Resources/election_data.csv'
-poll_list = []
+poll_csv_input = pathlib.Path('./Resources/election_data.csv')
+poll_analysis_output = pathlib.Path('./analysis/poll_analysis.csv')
+# poll_list = []
 poll_dict = {}
-with open(poll_file) as p:
-    pollreader = csv.reader(p, delimiter = ",")
+total_voters = 0
+
+with open(poll_csv_input) as poll_file:
+    pollreader = csv.reader(poll_file, delimiter = ",")
     pollheader = next(pollreader)
 
-    for index, row in enumerate(pollreader):
-        voter_id = row[0]
-        voter_county = row[1]
+    for row in pollreader:
+        # voter_id = row[0]
+        # voter_county = row[1]
         voter_candidate = row[2]
-        poll_list.append({voter_id : [voter_county, voter_candidate]})
+        # poll_list.append({voter_id : [voter_county, voter_candidate]})
+        # Tally votes with polling dictionary
         if voter_candidate in poll_dict:
             poll_dict[voter_candidate] = poll_dict[voter_candidate] + 1
         else:
             poll_dict[voter_candidate] = 1
+        total_voters += 1
+# print(poll_dict)
 
+# Print out the results
 # The total number of votes cast
-total_voters = len(poll_list)
 print(f"Total voter records: {total_voters}")
 
 # A complete list of candidates who received votes
-# print(poll_dict)
-for candidate in poll_dict:
-    print(f"{candidate}:  {poll_dict[candidate]}")
-
 # The percentage of votes each candidate won
-
 # The total number of votes each candidate won
+votes_to_beat = 0
+for candidate in poll_dict:
+    pct_votes = poll_dict[candidate] * 100 / total_voters
+    print(f"{candidate:10}:  {pct_votes:8.3f}% ({poll_dict[candidate]} votes)")
+    if poll_dict[candidate] > votes_to_beat:
+        winner        = candidate
+        votes_to_beat = poll_dict[candidate]
 
 # The winner of the election based on popular vote.
+print(f"Winner : {winner}")
